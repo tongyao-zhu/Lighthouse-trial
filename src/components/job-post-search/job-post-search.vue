4c0fac49b8d20651595285a9924af682dc416ae9
Tongyao <tongyaozhu1@gmail.com>
2019-03-17 22:19:53 +0800

    Added job post search

---
+++ src/components/job-post-search/job-post-search.vue
@@ -0,0 +1,40 @@
+<template src="job-post-search.html"></template>
+
+<script>
+import eachPost from './each-post';
+
+export default {
+  name: 'JobPosts',
+  components: {
+    eachPost,
+  },
+  data() {
+    return {
+      positions: [
+
+        {
+          "position_token": "88b69a350549efb3604bdf5a8439db4fd5fa7e1f5ccf11768e34e92e38eb151c",
+          "position_location": 'Singapore',
+          "logo_url": "https://img.icons8.com/color/96/000000/organization.png",
+          "position_name": "Software Engineer",
+          "company_name": "Google"
+        },
+        {
+          "position_token": "asdfsfsabjislfjdilwfjeoajskdlf1o23213123442031203ajskdjfkaslkdd",
+          "position_location": 'Singapore',
+          "logo_url": "https://img.icons8.com/color/96/000000/organization.png",
+          "position_name": "Software Engineer Internship",
+          "company_name": "JP Morgan"
+        },
+        {
+          "position_token": "asdfsfsabjislfjdilwfjeoajskdlf1o23213123442031203ajskdjfkaslkdd",
+          "position_location": 'Singapore',
+          "logo_url": "https://img.icons8.com/color/96/000000/organization.png",
+          "position_name": "Technology Engineer",
+          "company_name": "Goldman Sachs"
+        },
+      ],
+    };
+  },
+};
+</script>
