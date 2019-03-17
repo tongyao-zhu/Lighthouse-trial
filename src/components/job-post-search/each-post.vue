4e233fcbffa3d4a833b878ee6bbda08ace5bca78
Tongyao <tongyaozhu1@gmail.com>
2019-03-17 23:29:20 +0800

    Added Job Post Search Listings

--- src/components/job-post-search/each-post.vue
+++ src/components/job-post-search/each-post.vue
@@ -1,36 +1,27 @@
 <template>
-  <div style = 'background-color:rgb(227,234,245)'> 
-
-  <img v-bind:src= "position.logo_url"
-  v-bind:alt="position.position_name"
-  align = "left" 
-  style = 'height:115px;width:115px'
-  v-bind:title = "position.position_name">
-  
-  <b style = 'font-size:150%'>
-  {{position.position_name}}</b>
-  <br/>
-  
-  <p style = 'color:rgb(118,113,113)'>
-  {{position.position_location}}</p>
-
-  <p style = 'font-size:120%;color:rgb(118,113,113)'>
-  {{position.company_name}} </p>
-
+  <div style='background-color:rgb(227,234,245)'>
+    <img v-bind:src="position.logo_url" v-bind:alt="position.position_name"
+    align="left"
+    style='height:110px;width:110px'
+    v-bind:title="position.position_name">
+    <b style='font-size:150%'>
+      {{position.position_name}}</b>
+    <br />
+    <p style='color:rgb(118,113,113)'>
+      {{position.position_location}}</p>
+    <p style='font-size:120%;color:rgb(118,113,113)'>
+      {{position.company_name}}</p>
   </div>
-
 </template>
-
-
 <script>
-export default{
-	name:'EachPost',
-	props:{
-		position:{
-			type:Object,
-			required:true,
-		},
-	},
+export default {
+  name: 'EachPost',
+  props: {
+    position: {
+      type: Object,
+      required: true,
+    },
+  },
 };
 
 </script>
