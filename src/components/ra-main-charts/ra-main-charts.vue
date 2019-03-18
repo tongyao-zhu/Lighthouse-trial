8f549fe6d8710502bfaa008eab67577c1963389a
zhirui <zhangzhirui117@gmail.com>
2019-03-18 21:24:33 +0800

    Add basic skeleton for bar chart

---
+++ src/components/ra-main-charts/ra-main-charts.vue
@@ -0,0 +1,17 @@
+<template src="./ra-main-charts.html"></template>
+<style src="./ra-main-charts.scss" lang="scss"></style>
+
+<script>
+import BarChart from '@/components/ra-main-bar-chart/ra-main-bar-chart'
+import HighchartsVue from 'highcharts-vue';
+import Highcharts from "highcharts";
+
+export default {
+  name: 'OpeningPositions',
+  components: {
+    BarChart,
+  },
+};
+
+
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import App from './app/app';
 import router from './app/router/router';
+importimport Vue from 'vue';
+import HighchartsVue from 'highcharts-vue';
 import './styles/main.scss';
 
 Vue.config.productionTip = process.env.NODE_ENV === 'production';
