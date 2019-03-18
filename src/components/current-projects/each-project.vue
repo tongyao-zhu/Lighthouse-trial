f99146198552a0ae6fdb0b2c5ab26b46ef9d29d1
Tongyao <tongyaozhu1@gmail.com>
2019-03-19 00:47:55 +0800

    Added Current Projects (Clickable)

---
+++ src/components/current-projects/each-project.vue
@@ -0,0 +1,19 @@
+<template src='./each-project.html'></template>
+<style src='./each-project.scss' lang='scss'></style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<script>
+export default {
+  name: 'EachProject',
+  props: {
+    project: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+
+</script>
+
