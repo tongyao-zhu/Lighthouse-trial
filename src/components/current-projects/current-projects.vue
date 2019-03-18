319d40959121706c4c79240b82531fdd33cd12b5
Tongyao <tongyaozhu1@gmail.com>
2019-03-19 00:48:20 +0800

    added current projects"

--- src/components/current-projects/current-projects.vue
+++ src/components/current-projects/current-projects.vue
@@ -15,25 +15,28 @@ export default {
         {
           title: 'Crowd-based Innovation: a modern approach',
           description: 'New generation distributed computing systems often consist of co-operating components, and make use of object middleware technology. Due to their requirements, it is usually not acceptable to cause major interruptions in the service provided by these systems.',
-          requirments: ['Good programming skills', 'Excellent communication skills'],
+          requirements: ['Good programming skills', 'Excellent communication skills'],
           period: 'Jun, 2019 - Dec, 2019',
-          contact: 'John@u.nus.edu'
+          person: 'Jonh Joe',
+          contact: 'John@u.nus.edu',
         },
 
         {
           title: 'Crowdfunding and Entrepreneurial Learning',
           description: 'The proposed architecture should minimise the requirements on the application layer. it is usually not acceptable to cause major interruptions in the service provided by these systems.',
-          requirments: ['Good programming skills', 'Excellent communication skills'],
+          requirements: ['Good programming skills', 'Excellent communication skills'],
           period: 'Jun, 2019 - Dec, 2019',
-          contact: 'John@u.nus.edu'
+          person: 'Jonh Joe',
+          contact: 'John@u.nus.edu',
         },
 
         {
           title: 'Blockchain Governance and Consensus Mechanisms',
           description: 'The objective of this assignment is to define an architecture and sign of middleware support for reconfiguring CORBA-based distributed systems at run-time',
-          requirments: ['Good programming skills', 'Excellent communication skills'],
+          requirements: ['Good programming skills', 'Excellent communication skills'],
           period: 'Jun, 2019 - Dec, 2019',
-          contact: 'John@u.nus.edu'
+          person: 'Jonh Joe',
+          contact: 'John@u.nus.edu',
         },
       ],
     };
--- src/components/current-projects/each-project.vue
+++ src/components/current-projects/each-project.vue
@@ -1,8 +1,6 @@
 <template src='./each-project.html'></template>
 <style src='./each-project.scss' lang='scss'></style>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
 
 <script>
 export default {
@@ -12,6 +10,14 @@ export default {
       type: Object,
       required: true,
     },
+  },
+  methods:{
+  	sendMail:function(){
+  		var str1='mailto:';
+  		var str2=this.project.contract;
+  		print(str1.concat(st2));
+  		return str1.concat(str2);
+  	},
   },
 };
 
