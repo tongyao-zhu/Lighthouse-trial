cfdabceb0d8f1b263abf541c984ff67c849c0842
zhirui <zhangzhirui117@gmail.com>
2019-03-18 14:08:39 +0800

    Add opening-positions basic skeleton

---
+++ src/components/opening-positions/each-position.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <i class="far fa-address-book"></i>
+    align='left'
+    class = 'position-icon'>
+    <i>{{description}}</i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'eachPosition',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ src/components/opening-positions/opening-positions.vue
@@ -0,0 +1,25 @@
+<template src="./opening-postions.html"></template>
+<style src="./opening-positions.scss" lang="scss"></style>
+<script>
+
+import eachPosition from './each-position';
+
+export default {
+  name: 'OpeningPositions',
+  components:{
+    eachPosition
+  },
+
+  data(){
+    return{
+      positions:[
+        "Data Centre Technician Internship",
+        "Financial Analyst, Media Incentives",
+        "Global Marketing Analyst, Media Lab",
+        "Operations Specialist, Google People Service",
+        "Software Engineer, Front End Development"
+      ]
+    }
+  }
+};
+</script>
