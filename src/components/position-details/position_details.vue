49156cc0eb22b5c2a524d7d4d463e2b348efc384
Yang Xiya <xiya@u.nus.edu>
2019-03-20 15:02:14 +0800

    add position-details

---
+++ src/components/position-details/position_details.vue
@@ -0,0 +1,21 @@
+<template src="./position-details.html"></template>
+<style src="./position-details.scss" lang="scss"></style>
+
+<script>
+import BarChart from '@/components/charts/bar';
+import AreaChart from '@/components/charts/area';
+import HorizontalBarChart from '@/components/charts/horizontal-bar';
+import Boxplot from '@/components/charts/boxplot';
+
+export default {
+  name: 'IndustryExplorer',
+  components: {
+    BarChart,
+    AreaChart,
+    HorizontalBarChart,
+    Boxplot,
+  },
+};
+
+
+</script>
