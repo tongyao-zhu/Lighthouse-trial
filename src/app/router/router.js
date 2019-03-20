5079ef7ccfbaddd427b4c59d8dec827c1c80cdf3
JacobWangTengda <wtd8207@gmail.com>
2019-03-20 18:32:52 +0800

    Modify router.js

--- src/app/router/router.js
+++ src/app/router/router.js
@@ -40,7 +40,7 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      component: TrejectoryPosition,
+      component: About,
     },
     {
       path: '/position-stats',
